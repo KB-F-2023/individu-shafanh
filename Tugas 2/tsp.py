852f07cc4fe8a998e5ead82b32b40a46cf5c9214
shafanh <120880399+shafanh@users.noreply.github.com>
2023-06-19 20:18:01 +0700

    Add files via upload

---
+++ Tugas 2/tsp.py
@@ -0,0 +1,108 @@
+import random
+
+# Define the cities and their coordinates
+cities = {
+    'A': (0, 0),
+    'B': (1, 3),
+    'C': (2, 1),
+    'D': (5, 0),
+    'E': (6, 3),
+    'F': (7, 1),
+    'G': (10, 0),
+    'H': (11, 3),
+    'I': (12, 1),
+    'J': (15, 0)
+}
+
+# Define the genetic algorithm parameters
+POPULATION_SIZE = 20
+NUM_GENERATIONS = 1000
+MUTATION_RATE = 0.1
+
+# Define the fitness function
+# menghitung jarak rute yang mengunjungi semua kota dalam urutan yang diberikan. 
+# Ini mengembalikan invers dari jarak sebagai skor fitness
+def get_fitness(route):
+    distance = 0
+    for i in range(len(route)):
+        from_city = cities[route[i]]
+        to_city = cities[route[(i + 1) % len(route)]]
+        distance += ((from_city[0] - to_city[0]) ** 2 + (from_city[1] - to_city[1]) ** 2) ** 0.5
+    return 1 / distance
+
+# Define the create initial population function
+# membuat populasi awal rute acak
+def create_population():
+    population = []
+    for i in range(POPULATION_SIZE):
+        route = list(cities.keys())
+        random.shuffle(route)
+        population.append(route)
+    return population
+
+# Define the breed function
+# membuat rute baru yang menggabungkan karakteristik dari kedua parent
+def breed(parent1, parent2):
+    child = [''] * len(parent1)
+    gene1 = random.randint(0, len(parent1) - 1)
+    gene2 = random.randint(0, len(parent1) - 1)
+    start_gene = min(gene1, gene2)
+    end_gene = max(gene1, gene2)
+    for i in range(start_gene, end_gene + 1):
+        child[i] = parent1[i]
+    for i in range(len(parent2)):
+        if parent2[i] not in child:
+            for j in range(len(child)):
+                if child[j] == '':
+                    child[j] = parent2[i]
+                    break
+    return child
+
+# Define the mutate function
+# menukar dua kota dalam rute tertentu dengan probabilitas yang ditentukan oleh tingkat mutasi
+def mutate(route):
+    if random.random() < MUTATION_RATE:
+        gene1 = random.randint(0, len(route) - 1)
+        gene2 = random.randint(0, len(route) - 1)
+        route[gene1], route[gene2] = route[gene2], route[gene1]
+    return route
+
+# Define the evolve function
+# dua rute teratas berdasarkan skor fitness mereka, melakukan breeding dan mutasi untuk menciptakan rute baru hingga ukuran populasi tercapai
+def evolve(population):
+    # Select the top 2 routes
+    sorted_population = sorted(population, key=get_fitness, reverse=True)
+    parents = sorted_population[:2]
+
+    # Breed and mutate to create new routes
+    offspring = [parents[0], parents[1]]
+    while len(offspring) < POPULATION_SIZE:
+        child = breed(parents[0], parents[1])
+        child = mutate(child)
+        offspring.append(child)
+
+    return offspring
+
+# Define the main function
+def main():
+    # Create the initial population
+    population = create_population()
+
+    # Evolve the population for a number of generations
+    for i in range(NUM_GENERATIONS):
+        population = evolve(population)
+
+    # Calculate the fitness of each individual in the final population
+    fitness_scores = []
+    for individual in population:
+        fitness_scores.append(get_fitness(individual))
+
+    # Sort the population by their fitness
+    sorted_population = [x for _, x in sorted(zip(fitness_scores, population), reverse=True)]
+
+    # Print the best route
+    best_route = sorted_population[0]
+    print("Best route:", best_route)
+
+if __name__ == '__main__':
+    main()
---
+++ Tugas 3/mapcoloring.py
@@ -0,0 +1,63 @@
+def color_map(adj_list, colors):
+    # adj_list adalah kamus yang mewakili daftar kedekatan grafik
+    # colors adalah daftar warna yang tersedia
+    
+    # Inisialisasi color map tanpa warna yang ditetapkan
+    color_map = {node: None for node in adj_list.keys()}
+    
+    # Tentukan fungsi pembantu untuk memeriksa apakah suatu warna valid untuk node yang diberikan
+    def is_valid_color(node, color):
+        for neighbor in adj_list[node]:
+            if color_map[neighbor] == color:
+                return False
+        return True
+    
+    # Tentukan fungsi backtracking rekursif
+    def backtrack(node):
+        # Base case: jika semua node telah diberi warna, return True
+        if node is None:
+            return True
+        
+        # Coba tetapkan setiap warna ke node saat ini
+        for color in colors:
+            if is_valid_color(node, color):
+                color_map[node] = color
+                if backtrack(next_node()):
+                    return True
+                color_map[node] = None
+        
+        # Jika tidak ada penetapan warna yang mengarah ke solusi, backtrack
+        return False
+    
+    # Tentukan fungsi untuk mendapatkan node tidak berwarna berikutnya
+    def next_node():
+        for node, color in color_map.items():
+            if color is None:
+                return node
+        return None
+    
+    # Mulai algoritma backtracking dari node pertama
+    backtrack(next_node())
+    
+    return color_map
+
+# Tentukan daftar adjacency untuk grafik 
+adj_list = {
+    'WA': ['NT', 'SA'],
+    'NT': ['WA', 'SA', 'Q'],
+    'Q': ['NT', 'SA', 'NSW'],
+    'NSW': ['Q', 'SA', 'V'],
+    'V': ['NSW', 'SA'],
+    'SA': ['WA', 'NT', 'Q', 'NSW', 'V', 'T'],
+    'T': ['SA']
+}
+
+# Tentukan daftar warna yang tersedia
+colors = ['red', 'green', 'blue']
+
+# Menyelesaikan coloring problem
+color_map = color_map(adj_list, colors)
+
+# Print hasil
+print(color_map)
+
